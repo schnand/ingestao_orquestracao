f3b3a24bbb3c09243a136172148d74cc6a64e0b5
schnand <schneider.and146@gmail.com>
2023-04-26 14:45:44 -0300

    ...

---
+++ 01_consumer.py
@@ -0,0 +1,22 @@
+import pika
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters('localhost')
+)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+channel.basic_consume(queue='hello',
+                    #auto_ack=True,
+                    on_message_callback=callback,
+                    arguments={'x-stream-offset': 'first'}
+                    #arguments={'x-stream-offset': 'last'}
+                    )
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
